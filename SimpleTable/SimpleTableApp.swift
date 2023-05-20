7b7abd42a03088b5e5d8720323f6ce4a2445e2b2
lhhuy <lhh206198@gmail.com>
2023-05-21 01:23:19 +0700

    Initial Commit

---
+++ SimpleTable/SimpleTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableApp.swift
+//  SimpleTable
+//
+//  Created by LHH on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
